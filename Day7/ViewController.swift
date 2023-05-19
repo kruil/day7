6a42f8d00d6e48d85692426a83635a9638ae91b2
Ilia Krupko <ikrupko@ourgapps.com>
2023-05-19 16:38:59 +0400

    Done

--- Day7/ViewController.swift
+++ Day7/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    let imageView = UIImageView()
+    
+    let imageHeight = CGFloat(270)
+    
+    var topMargin: CGFloat {
+        view.safeAreaInsets.top
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        view.addSubview(scrollView)
+        scrollView.frame = view.frame
+        scrollView.contentSize = .init(width: view.frame.width, height: 1500)
+        scrollView.contentInsetAdjustmentBehavior = .never
+        scrollView.scrollIndicatorInsets = .init(
+            top: imageHeight - topMargin,
+            left: 0,
+            bottom: 0,
+            right: 0
+        )
+        scrollView.delegate = self
+        
+        imageView.frame = .init(x: 0, y: 0, width: view.frame.width, height: 270)
+        imageView.image = UIImage(named: "tiles")
+        imageView.contentMode = .scaleAspectFill
+        scrollView.addSubview(imageView)
     }
-
-
 }
 
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let yOffset = scrollView.contentOffset.y
+        if yOffset < 0 {
+            imageView.frame = .init(
+                x: 0,
+                y: yOffset,
+                width: view.frame.width,
+                height: imageHeight - yOffset
+            )
+            scrollView.scrollIndicatorInsets = .init(
+                top: imageHeight - topMargin - yOffset,
+                left: 0,
+                bottom: 0,
+                right: 0
+            )
+        }
+    }
+}
